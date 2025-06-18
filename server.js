3b5d17218844a8141db5718f8e411624112a6a18
Andrei Novozhilov <124013363+Postaler@users.noreply.github.com>
2025-06-18 17:38:16 +0000

    working

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ const port = 3000;
 //create instance of Express app
 const app = express();
 
+const session = require('express-session');
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }));
+
+//pass session data to routes
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
 app.set('view engine','ejs');
 
 
